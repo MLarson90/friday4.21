18b739ef333b37b9ad717e7e511cb870849391d7
Max Larson <mr.m.larson90>
2017-04-21 13:04:08 -0700

    solved minor api issue

--- js/doctor.js
+++ js/doctor.js
@@ -13,7 +13,7 @@ function Doctor(first, last, img , bio, city, state, street, street2){
 
 
 Doctor.prototype.findDocByCondition = function(condition, location, displayDoc){
-   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + condition + '&&limit=5&&location=' + location + '&&user_key=' +  apiKey).then(function(response){
+   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + condition + '&limit=5&location=' + location + '&user_key=' +  apiKey).then(function(response){
      for(i=0;i<response.data.length;i++){
        doctor = new Doctor(first = response.data[i].profile.first_name, last = response.data[i].profile.last_name, img = response.data[i].profile.image_url, bio = response.data[i].profile.bio );
        if(response.data[i].practices !== undefined){
